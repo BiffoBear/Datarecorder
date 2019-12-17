b7c086c9fe1b697322fe9f1b0548d1da62f0c713
BiffoBear <martin@martinstehens.net>
2019-12-17 07:52:49 +0300

    Refactored with project name datarecorder and a setup.py file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datarecorder',
+      version='0.1',
+      description='House data recorder',
+      url=None,
+      author='Martin Stephens',
+      author_email='martin@martinstephens.net',
+      license='MIT',
+      packages=['datarecorder'],
+      zip_safe=False)
