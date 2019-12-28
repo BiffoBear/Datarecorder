d2a0d5b40d1a92352418969dc7faa1a8ef64f237
BiffoBear <martin@martinstehens.net>
2019-12-29 00:32:10 +0700

    Turned off logging settings so it all goes to the console.

--- datarecorder.py
+++ datarecorder.py
@@ -37,22 +37,23 @@ def rfm69_callback(rfm69_irq):
 
 
 def initialize_logging():
+    pass
     # create file handler which logs even debug messages
     # set up logging to file - see previous section for more details
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s: %(name)-12s: %(levelname)-8s: %(message)s',
-                        datefmt='%y-%m-%d %H:%M',
-                        filename='testing.log',  # '/temp/myapp.log',
-                        filemode='w')
-    # define a Handler which writes INFO messages or higher to the sys.stderr
-    console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
-    # set a format which is simpler for console use
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    # tell the handler to use this format
-    console.setFormatter(formatter)
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(asctime)s: %(name)-12s: %(levelname)-8s: %(message)s',
+    #                     datefmt='%y-%m-%d %H:%M',
+    #                     filename='testing.log',  # '/temp/myapp.log',
+    #                     filemode='w')
+    # # define a Handler which writes INFO messages or higher to the sys.stderr
+    # console = logging.StreamHandler()
+    # console.setLevel(logging.DEBUG)
+    # # set a format which is simpler for console use
+    # formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # # tell the handler to use this format
+    # console.setFormatter(formatter)
     # add the handler to the root logger
-    logging.getLogger('').addHandler(console)
+    # logging.getLogger('').addHandler(console)
 
 
 def initialize_rfm69():
