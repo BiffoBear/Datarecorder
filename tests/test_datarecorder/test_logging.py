2fb78901eeb5b31751ca3171a0c4535a8052e347
BiffoBear <martin@martinstehens.net>
2020-01-28 12:25:13 +0300

    Refactored unittests into folders for datarecorder, radiohelper and databasehelper

--- tests/test_datarecorder/test_logging.py
+++ tests/test_datarecorder/test_logging.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from unittest import TestCase, skip
+from unittest import TestCase
 from unittest.mock import Mock, patch
 import pathlib
 import logging
