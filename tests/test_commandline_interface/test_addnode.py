9cf9e0cda5b3cde0de27b28392c180a0997b8c29
BiffoBear <martin@martinstehens.net>
2020-02-04 07:48:55 +0300

    Refactored addnode.py to nodes.py and commandline.addnode.py to commandlinetools.py

--- tests/test_commandline_interface/test_addnode.py
+++ tests/test_commandline_interface/test_addnode.py
@@ -1,15 +1,15 @@
 from unittest import TestCase, skip
 from unittest.mock import Mock, patch, call
 from sqlalchemy.orm.exc import NoResultFound
-from commandline import addnode
+from commandline import commandlinetools
 
 
 class TestListNodesAndSensors(TestCase):
 
     def test_high_lite_existing_things(self):
-        returned_result = addnode._high_lite_existing_things(thing=5, existing_things=[5])
+        returned_result = commandlinetools._high_lite_existing_things(thing=5, existing_things=[5])
         self.assertEqual(f'\x1b[1m05 \x1b[0m', returned_result)
-        returned_result = addnode._high_lite_existing_things(thing=5, existing_things=[4])
+        returned_result = commandlinetools._high_lite_existing_things(thing=5, existing_things=[4])
         self.assertEqual(f'\x1b[90m05 \x1b[0m', returned_result)
 
     def test_print_existing_things_displays_correct_things(self):
@@ -49,21 +49,21 @@ class TestListNodesAndSensors(TestCase):
 [0m[90mfa [0m[90mfb [0m[90mfc [0m[90mfd [0m[90mfe [0m
 
 '''
-        returned_result = addnode._layout_existing_things(thing_name='sensor', existing_things=[1, 5])
+        returned_result = commandlinetools._layout_existing_things(thing_name='sensor', existing_things=[1, 5])
         self.assertEqual(expected_result, returned_result)
 
     def test_layout_existing_things_handles_empty_lists(self):
         expected_result = 'No existing nodes in database'
-        returned_result = addnode._layout_existing_things('node', [])
+        returned_result = commandlinetools._layout_existing_things('node', [])
         self.assertEqual(expected_result, returned_result)
         expected_result = 'No existing sensors in database'
-        returned_result = addnode._layout_existing_things('sensor', [])
+        returned_result = commandlinetools._layout_existing_things('sensor', [])
         self.assertEqual(expected_result, returned_result)
 
     def test_convert_value_to_string(self):
-        self.assertEqual('0x02', addnode._convert_value_to_string(2))
-        self.assertEqual('0x03', addnode._convert_value_to_string(3))
-        self.assertEqual('test string', addnode._convert_value_to_string('test string'))
+        self.assertEqual('0x02', commandlinetools._convert_value_to_string(2))
+        self.assertEqual('0x03', commandlinetools._convert_value_to_string(3))
+        self.assertEqual('test string', commandlinetools._convert_value_to_string('test string'))
 
     def test_layout_thing_details(self):
         data = {'Sensor_ID': 1,
@@ -79,7 +79,7 @@ Name -- Sensor name
 Quantity -- Temperature
 
 '''
-        returned_result = addnode._layout_thing_details(thing_name='sensor', thing_id=1, thing_data=data)
+        returned_result = commandlinetools._layout_thing_details(thing_name='sensor', thing_id=1, thing_data=data)
         self.assertEqual(expected_result, returned_result)
         data = {'Node_ID': 2,
                 'Name': 'Node name',
@@ -92,19 +92,19 @@ Name -- Node name
 Location -- A location
 
 '''
-        returned_result = addnode._layout_thing_details(thing_name='node', thing_id=2, thing_data=data)
+        returned_result = commandlinetools._layout_thing_details(thing_name='node', thing_id=2, thing_data=data)
         self.assertEqual(expected_result, returned_result)
 
 
 class TestHelperFunctions(TestCase):
 
     @patch('builtins.print')
-    @patch('commandline.addnode._layout_existing_things', autospec=True)
+    @patch('commandline.commandlinetools._layout_existing_things', autospec=True)
     @patch('database.database.get_all_node_ids', autospec=True)
     def test_list_nodes(self, mock_get_all_nodes, mock_layout_existing_things, mock_print):
         mock_get_all_nodes.return_value = [1, 2, 3]
         mock_layout_existing_things.return_value = 'Hello World'
-        addnode.list_nodes()
+        commandlinetools.list_nodes()
         mock_get_all_nodes.assert_called_once()
         mock_layout_existing_things.assert_called_once_with(thing_name='node',
                                                             existing_things=[1, 2, 3]
@@ -116,7 +116,7 @@ class TestHelperFunctions(TestCase):
         mock_print.reset_mock()
         mock_get_all_nodes.return_value = [4, 5, 6]
         mock_layout_existing_things.return_value = 'Hello Mars'
-        addnode.list_nodes()
+        commandlinetools.list_nodes()
         mock_get_all_nodes.assert_called_once()
         mock_layout_existing_things.assert_called_once_with(thing_name='node',
                                                             existing_things=[4, 5, 6]
@@ -124,12 +124,12 @@ class TestHelperFunctions(TestCase):
         mock_print.assert_called_once_with('Hello Mars')
 
     @patch('builtins.print')
-    @patch('commandline.addnode._layout_thing_details', autospec=True)
+    @patch('commandline.commandlinetools._layout_thing_details', autospec=True)
     @patch('database.database.get_node_data', autospec=True)
     def test_show_node_details(self, mock_get_node_data, mock_layout_thing_details, mock_print):
         mock_get_node_data.return_value = {'a': 1}
         mock_layout_thing_details.return_value = 'Hello World'
-        addnode.show_node_details(node_id=100)
+        commandlinetools.show_node_details(node_id=100)
         mock_get_node_data.assert_called_once_with(100)
         mock_layout_thing_details.assert_called_once_with(thing_name='node', thing_id=100, thing_data={'a': 1})
         mock_print.assert_called_once_with('Hello World')
@@ -139,25 +139,25 @@ class TestHelperFunctions(TestCase):
         mock_print.reset_mock()
         mock_get_node_data.return_value = {'b': 2}
         mock_layout_thing_details.return_value = 'Hello Mars'
-        addnode.show_node_details(node_id=95)
+        commandlinetools.show_node_details(node_id=95)
         mock_get_node_data.assert_called_once_with(95)
         mock_layout_thing_details.assert_called_once_with(thing_name='node', thing_id=95, thing_data={'b': 2})
         mock_print.assert_called_once_with('Hello Mars')
 
         mock_get_node_data.side_effect = NoResultFound('error message')
         mock_print.reset_mock()
-        addnode.show_node_details(node_id=95)
+        commandlinetools.show_node_details(node_id=95)
         mock_print.assert_called_once_with('Error message')
 
         mock_get_node_data.side_effect = NoResultFound('error message 2')
         mock_print.reset_mock()
-        addnode.show_node_details(node_id=95)
+        commandlinetools.show_node_details(node_id=95)
         mock_print.assert_called_once_with('Error message 2')
 
     @patch('builtins.print')
     @patch('database.database.add_node', autospec=True)
     def test_add_node_to_database(self, mock_add_node_to_database, mock_print):
-        addnode.add_node_to_database(node_id=34, name='Test name', location='Test location')
+        commandlinetools.add_node_to_database(node_id=34, name='Test name', location='Test location')
         mock_add_node_to_database.assert_called_once_with(node_id=34,
                                                           name='Test name',
                                                           location='Test location',
@@ -167,10 +167,10 @@ class TestHelperFunctions(TestCase):
         mock_add_node_to_database.side_effect = [ValueError('error message'),
                                                  TypeError('other error message'),
                                                  ]
-        addnode.add_node_to_database(node_id=34, name='Test name', location='Test location')
+        commandlinetools.add_node_to_database(node_id=34, name='Test name', location='Test location')
         mock_print.assert_called_once_with('Unable to create node -- error message\n')
         mock_print.reset_mock()
-        addnode.add_node_to_database(node_id=34, name='Test name', location='Test location')
+        commandlinetools.add_node_to_database(node_id=34, name='Test name', location='Test location')
         mock_print.assert_called_once_with('Unable to create node -- other error message\n')
 
 
@@ -179,19 +179,19 @@ class TestSetupArgparseForNodes(TestCase):
     @patch('argparse.ArgumentParser')
     def test_argparse_setup_for_nodes(self, mock_argparse):
         mock_argparse.return_value = Mock()
-        mock_parser = addnode.setup_node_argparse()
+        mock_parser = commandlinetools.setup_node_argparse()
         mock_argparse.assert_called_once()
         mock_parser.add_subparsers.assert_called_once_with(
             help='commands to add nodes and display information about nodes')
         calls = [call(help='commands to add nodes and display information about nodes'),
                  call().add_parser('list', help='list all existing nodes'),
-                 call().add_parser().set_defaults(func=addnode.list_nodes),
+                 call().add_parser().set_defaults(func=commandlinetools.list_nodes),
                  call().add_parser('show', help='display information for the node'),
                  call().add_parser().add_argument('id', type=int,
                                                   help='id for the node to display, an integer in range 0-254'),
-                 call().add_parser().set_defaults(func=addnode.show_node_details),
+                 call().add_parser().set_defaults(func=commandlinetools.show_node_details),
                  call().add_parser('add', help='display information for the node'),
-                 call().add_parser().set_defaults(func=addnode.add_node_to_database),
+                 call().add_parser().set_defaults(func=commandlinetools.add_node_to_database),
                  call().add_parser().add_argument('id', type=int,
                                                   help='id for the node to add, an integer in range 0-254'),
                  call().add_parser().add_argument('name', help='name for the node to add'),
@@ -199,23 +199,23 @@ class TestSetupArgparseForNodes(TestCase):
                  ]
         mock_parser.add_subparsers.assert_has_calls(calls)
 
-    @patch('commandline.addnode.list_nodes', autospec=True)
+    @patch('commandline.commandlinetools.list_nodes', autospec=True)
     def test_parser_calls_list_node_function(self, mock_list_nodes):
-        test_parser = addnode.setup_node_argparse()
+        test_parser = commandlinetools.setup_node_argparse()
         args = test_parser.parse_args(['list'])
         args.func()
         mock_list_nodes.assert_called_once()
 
-    @patch('commandline.addnode.show_node_details', autospec=True)
+    @patch('commandline.commandlinetools.show_node_details', autospec=True)
     def test_parser_calls_show_node_details_function(self, mock_show_node_details):
-        test_parser = addnode.setup_node_argparse()
+        test_parser = commandlinetools.setup_node_argparse()
         args = test_parser.parse_args(['show', '2'])
         args.func(node_id=args.id)
         mock_show_node_details.assert_called_once_with(node_id=2)
 
-    @patch('commandline.addnode.add_node_to_database', autospec=True)
+    @patch('commandline.commandlinetools.add_node_to_database', autospec=True)
     def test_parser_calls_add_node(self, mock_add_node):
-        test_parser = addnode.setup_node_argparse()
+        test_parser = commandlinetools.setup_node_argparse()
         args = test_parser.parse_args(['add', '2', 'Node Name', 'Node location'])
         args.func(node_id=args.id, name=args.name, location=args.location)
         mock_add_node.assert_called_once_with(node_id=2, name='Node Name', location='Node location')
