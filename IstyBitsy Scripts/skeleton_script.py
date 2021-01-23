bce9367fe72d6f6196fb08873b789f26f47ff0df
BiffoBear <martin@martinstehens.net>
2021-01-23 09:49:15 +0700

    Added constants and version number to skeleton_script.py.

--- IstyBitsy Scripts/skeleton_script.py
+++ IstyBitsy Scripts/skeleton_script.py
@@ -1,12 +1,23 @@
+# Version 1.0 2021-01-23
+# Required for all nodes.
 from node_radio import node_radio
 import board
+# Specific to this node.
 
+# Setup radio and status LED
+RESET_PIN = board.D7
+CS_PIN = board.D9
+LED_PIN = board.D13
 NODE_ID = 0xfe
-SEND_PERIOD = 15
+SEND_PERIOD = 60  # seconds
 
-radio = node_radio.Radio(cs_pin=board.D7, reset_pin=board.D5, led_pin=board.D13,
+radio = node_radio.Radio(cs_pin=CS_PIN, reset_pin=RESET_PIN, led_pin=LED_PIN,
                          node_id=NODE_ID, send_period=SEND_PERIOD)
 
+# Setup node speciic sensors, etc.
+i2c = board.I2C()
+
+# Main loop
 radio.initial_sleep()
 while True:
     radio.timer_reset()
