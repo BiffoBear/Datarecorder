81f8fc103d9e197835c376cf193abc79674ad6bc
BiffoBear <martin@martinstehens.net>
2021-02-03 23:29:10 +0000

    Sketched out event handler thread

---
+++ datarecorder/_handleevents.py
@@ -0,0 +1,42 @@
+import logging
+import threading
+import queue
+import urllib.request
+
+event_queue = queue.Queue()
+
+def read_event_queue_handle_event():
+    logger.debug('read_event_queue_handle_event called')
+    global event_queue
+    try:
+        event = event_queue.get()
+        # display = add_screen_line(display=display, text=text)
+        # if display['oled'] is not None:
+        #     display = draw_lines(display=display)
+    except queue.Empty:
+        logger.error('Event thread called with empty queue')
+    finally:
+        event_queue.task_done()
+
+
+def write_event_to_queue(event=None):
+    logger.debug('write_event_to_queue called')
+    try:
+        event_queue.put_nowait(message_text)
+    except queue.Full:
+        pass
+
+
+def init_event_thread():
+    logger.debug(f'init_event_thread called')
+    event_thread = threading.Thread(target=loop_read_event_queue)
+    event_thread.daemon = True
+    event_thread.start()
+    return event_thread
+
+
+def loop_read_event_queue():
+    logger.debug(f'loop_read_event_queue called')
+    global global_display
+    while True:
+        global_display = read_message_queue_write_to_display(display=global_display)
