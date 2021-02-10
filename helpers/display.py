f92c980a6c55509d9d08a0375551b810ecabc9dc
BiffoBear <martinstephens@me.com>
2021-02-10 23:54:28 +0000

    Implemented message queue, display class and screen line buffer.

--- helpers/display.py
+++ helpers/display.py
@@ -1,6 +1,17 @@
+"""Writes messages to a small adafruit.com OLED display over an I2C bus."""
 import threading
 import queue
 from collections import deque
 
+message_queue = queue.Queue()
 
 
+class Display:
+    """Updates screen line buffer and writes to the display."""
+    pass
+    def __init__(self):
+        self._screen_line_buffer = deque([], maxlen=5)
+        
+    def _write_to_buffer(self, *, line=""):
+        self._screen_line_buffer.append(line)
+        
--- tests/test_helpers/test_display.py
+++ tests/test_helpers/test_display.py
@@ -1,1 +1,25 @@
-a
+import queue
+from collections import deque
+from helpers import display
+
+def test_display_has_a_queue():
+    assert isinstance(display.message_queue, queue.Queue)
+    
+def test_write_to_queue():
+    assert display.message_queue.empty()
+    display.message_queue.put("Hello World")
+    assert not display.message_queue.empty()
+    assert display.message_queue.get() == "Hello World"
+
+
+class TestDisplayClass:
+    
+    def test_display_class_exists_and_has_a_message_buffer(self):
+        oled = display.Display()
+        assert oled._screen_line_buffer.maxlen == 5
+        
+    def test_write_to_line_buffer_appends_on_the_right(self):
+        oled = display.Display()
+        [oled._write_to_buffer(line=text) for text in ["1", "2"]]
+        assert oled._screen_line_buffer == deque(["1", "2"])
+        
